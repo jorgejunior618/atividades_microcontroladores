5afb0ea35eb538260d595267f5eabf6c86d1ce62
jorgejunior618 <jorgejunior618@gmail.com>
2022-05-15 13:18:38 -0300

    Otimização do código

--- atividades_pic/programas/lcd_nome/lcd_nome.c
+++ atividades_pic/programas/lcd_nome/lcd_nome.c
@@ -11,32 +11,7 @@
 #define BITDado 1
 
 unsigned char nomeJorge[] = {
-  0b00000100,
-  0b00001010,
-  0b00000100,
-  0b00001111,
-  0b00000101,
-  0b00000010,
-  0b00000100,
-  0b00000111,
-  0b00000100,
-  0b00000101,
-
-  0b00000000,
-  0b00000010,
-
-  0b00000100,
-  0b00001010,
-  0b00000101,
-  0b00000101,
-  0b00000100,
-  0b00001110,
-  0b00000100,
-  0b00001001,
-  0b00000100,
-  0b00001111,
-  0b00000101,
-  0b00000010,
+  'J','O','R','G','E',' ','J','U','N','I','O','R'
 };
 
 void inicializarAmbiente(void) {
@@ -47,9 +22,11 @@ void inicializarAmbiente(void) {
 	OSCCON =  0b01000100; // Configura a frequencia do oscilador interno para 1MHz
 }
 
-void delay3ms(void) {
-  int i;
-  for (i = 0; i < 40; i++) {}
+void delayMs(int delay) {
+  int i, j;
+  for (i = 0; i < delay; i++){
+    for (j = 0; j < 10; j++) {}
+  }
 }
 
 void delayInicialLCD(void) {
@@ -57,72 +34,77 @@ void delayInicialLCD(void) {
   for (i = 0; i < 1380; i++) {}
 }
 
-void realizaClock(void) {
+void pulsoEnable(void) {
   ENABLE = 1;
   ENABLE = 0;
+  delayMs(1);
+}
+
+void enviarComandoLCD(unsigned char comando) {
+  RS = BITInstrucao;
+  DISPLAY = comando;
+  
+  pulsoEnable();
+}
+
+void enviarDadoLCD(unsigned char dado) {
+  RS = BITDado;
+  DISPLAY = dado;
+  
+  pulsoEnable();
 }
 
 void limpaDisplay(void) {
   RS = BITInstrucao;
 
   DISPLAY = 0b00000000;
-  realizaClock();
+  pulsoEnable();
   DISPLAY = 0b00000001;
-  realizaClock();
-  delay3ms();
+  pulsoEnable();
+  delayMs(2);
 }
 
 void inicializarLCD(void) {
 	// DISPLAY = 0;
   delayInicialLCD();
   // Define modo 4bits, 1 linha, font 5x8
-  RS = BITInstrucao;
-  DISPLAY = 0b00000010;
-  realizaClock();
-  DISPLAY = 0b00000010;
-  realizaClock();
-  DISPLAY = 0b00001000;
-  realizaClock();
-  delay3ms();
+  // Define modo 4bits, 1 linha, font 5x8
+  enviarComandoLCD(0b00000010);
+  enviarComandoLCD(0b00000010);
+  enviarComandoLCD(0b00001000);
+  delayMs(2);
 
   // Define o LCD ligado e cursor
-  DISPLAY = 0b00000000;
-  realizaClock();
-  DISPLAY = 0b00001110;
-  realizaClock();
-  delay3ms();
+  enviarComandoLCD(0b00000000);
+  enviarComandoLCD(0b00001110);
+  delayMs(2);
 
   limpaDisplay();
+  delayMs(2);
 
   // Modo de Entrada
-  DISPLAY = 0b00000000;
-  realizaClock();
-  DISPLAY = 0b00000110;
-  realizaClock();
-  delay3ms();
+  enviarComandoLCD(0b00000000);
+  enviarComandoLCD(0b00000110);
+  delayMs(2);
 }
 
 void exibirNome(void) {
-  
+  unsigned char i;
+
+  for (i = 0; i < 12; i++) {
+    enviarDadoLCD(nomeJorge[i]>>4);
+    enviarDadoLCD(nomeJorge[i]);
+    delayMs(70);
+  }
 }
 
 void main(void) {
-  int i = 0;
   inicializarAmbiente();
   inicializarLCD();
 
   while(1) {
-    RS = BITDado;
-    DISPLAY = nomeJorge[i];
-    realizaClock();
-    delayInicialLCD();
-    i++;
-
-    if (i > 23) {
-      delayInicialLCD();
-      limpaDisplay();
-      delayInicialLCD();
-      i = 0;
-    }
+    exibirNome();
+    delayMs(1000);
+    limpaDisplay();
   }
 }
